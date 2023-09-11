a57e122cc228fa4c72e4d9f9d18c97f26a2f2746
kmastrin <kmastrin@student.alamo.edu>
2023-09-10 22:31:26 -0500

    update

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var life = 100; // Global variable. Stream Stray Kids on Spotify and YouTube <3
-var name = "Stray Kids"; // String variable
+var group = "Stray Kids"; // String variable
+console.log(group);
+
+
 {
     let x = 8; // Block Scope
 }
-
-console.log("Stray Kids");
 
 const favmember = {firstname:"Han", lastname:"Jisung"}; // Object variable
